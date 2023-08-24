319d37523d19cb35c481691e3791f58e958716c4
Shruti <shrutichintawar8@gmail.com>
2023-08-24 12:56:15 +0530

    New commit

--- Backend/online_book_store/src/main/java/com/app/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
--- Backend/online_book_store/src/main/java/com/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
