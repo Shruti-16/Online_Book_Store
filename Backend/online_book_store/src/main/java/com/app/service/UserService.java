ee1b86359d7261a6488250d0dfe78bbaab184881
anupKanere <kanereanup@gmail.com>
2023-08-26 10:47:18 +0530

    frontend updating

--- Backend/online_book_store/src/main/java/com/app/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.app.service;
-
-import com.app.entities.User;
-
-public interface UserService {
-	public User addNewUser(User user);
-}
--- Test/online_book_store final/src/main/java/com/app/entities/PaymentStatus.java
+++
@@ -1,6 +0,0 @@
-package com.app.entities;
-
-public enum PaymentStatus {
-	DECLINED, SUCCESSFUL, PENDING
-
-}
